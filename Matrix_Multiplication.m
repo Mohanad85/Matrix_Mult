45c6ee4d67f26966e37aaca49607299c5846faa8
Mohanad Sawas <you@example.com>
2022-07-14 10:50:26 +0200

    After changing names from vector to Cartesian

--- Matrix_Multiplication.m
+++ Matrix_Multiplication.m
@@ -1,5 +1,5 @@
 # Matrix multiplication program
-# It takes 2 Euler Representation vectorx [x y z rx ry rz] and returns also Euler representation of A*B
+# It takes 2 Euler Representation cartesiantorx [x y z rx ry rz] and returns also Euler representation of A*B
 # This is meant to simulate the vision offset
 # Created by:     Mohanad Sawas
 # Created on:     July-2022
@@ -9,25 +9,25 @@
 
 clc
 format short g # to show the result without e format
-function MatForm= vec_to_matrix (VecForm)
-  rx= rotx(VecForm(1,4));
-  ry= roty(VecForm(1,5));
-  rz= rotz(VecForm(1,6));
+function MatForm= cartesian_to_matrix (CartesianForm)
+  rx= rotx(CartesianForm(1,4));
+  ry= roty(CartesianForm(1,5));
+  rz= rotz(CartesianForm(1,6));
   r=rz*ry*rx;
-  trans=[VecForm(1,1); VecForm(1,2);VecForm(1,3)];
+  trans=[CartesianForm(1,1); CartesianForm(1,2);CartesianForm(1,3)];
   MatForm=[r, trans];
   last_row=[0 0 0 1];
   MatForm=[MatForm; last_row];
 endfunction
 
 
-function MatRes= vec_multiplication (Vec1, Vec2)
-  Mat1= vec_to_matrix(Vec1);
-  Mat2= vec_to_matrix(Vec2);
+function MatRes= cartesian_multiplication (Cartesian1, Cartesian2)
+  Mat1= cartesian_to_matrix(Cartesian1);
+  Mat2= cartesian_to_matrix(Cartesian2);
   MatRes=Mat1*Mat2;
 endfunction
 
-function [VecForm_1,VecForm_2]= matrix_to_vector (MatForm)
+function [CartesianForm_1,CartesianForm_2]= matrix_to_cartesiantor (MatForm)
   x=MatForm(1,4);
   y=MatForm(2,4);
   z=MatForm(3,4);
@@ -53,50 +53,50 @@ function [VecForm_1,VecForm_2]= matrix_to_vector (MatForm)
     endif
 
   endif
-  VecForm_1(1,1)=VecForm_2(1,1)=round(100*x)/100;
-  VecForm_1(1,2)=VecForm_2(1,2)=round(100*y)/100;
-  VecForm_1(1,3)=VecForm_2(1,3)=round(100*z)/100;
-  VecForm_1(1,4)=round(100*rx1)/100;
-  VecForm_1(1,5)=round(100*ry1)/100;
-  VecForm_1(1,6)=round(100*rz1)/100;
-   VecForm_2(1,4)=round(100*rx2)/100;
-  VecForm_2(1,5)=round(100*ry2)/100;
-  VecForm_2(1,6)=round(100*rz2)/100; # round(100 * value) / 100
+  CartesianForm_1(1,1)=CartesianForm_2(1,1)=round(100*x)/100;
+  CartesianForm_1(1,2)=CartesianForm_2(1,2)=round(100*y)/100;
+  CartesianForm_1(1,3)=CartesianForm_2(1,3)=round(100*z)/100;
+  CartesianForm_1(1,4)=round(100*rx1)/100;
+  CartesianForm_1(1,5)=round(100*ry1)/100;
+  CartesianForm_1(1,6)=round(100*rz1)/100;
+  CartesianForm_2(1,4)=round(100*rx2)/100;
+  CartesianForm_2(1,5)=round(100*ry2)/100;
+  CartesianForm_2(1,6)=round(100*rz2)/100; # round(100 * value) / 100
   endfunction
 
 
 
 
-# multiply 2 vectors
-function ResVect= multiply_two_vectors(Vec1, Vec2)
-  MatForm= vec_multiplication (Vec1, Vec2);
-  ResVect=matrix_to_vector (MatForm);
+# multiply 2 cartesiantors
+function ResCartesiant= multiply_two_cartesiantors(Cartesian1, Cartesian2)
+  MatForm= cartesian_multiplication (Cartesian1, Cartesian2);
+  ResCartesiant=matrix_to_cartesiantor (MatForm);
     endfunction
 
 # function to calculate Voffset out of Reference position and Found Position
 function VOffset= calc_VOffset(RF,FP)
   #Voffset= FP:inv(RF)
-  RF_mat=vec_to_matrix(RF);
+  RF_mat=cartesian_to_matrix(RF);
   InvRF_mat=inv(RF_mat);
-  InvRF=matrix_to_vector(InvRF_mat);
-  VOffset_mat=vec_multiplication(FP,InvRF);
-  VOffset=matrix_to_vector(VOffset_mat);
+  InvRF=matrix_to_cartesiantor(InvRF_mat);
+  VOffset_mat=cartesian_multiplication(FP,InvRF);
+  VOffset=matrix_to_cartesiantor(VOffset_mat);
   endfunction
 
 
-function InvVect=inverse_vector(Vector)
-  MatForm= vec_to_matrix (Vector);
+function InvCartesiant=inverse_cartesiantor(Cartesiantor)
+  MatForm= cartesian_to_matrix (Cartesiantor);
   InvMatForm=inv(MatForm);
-  InvVect=matrix_to_vector (InvMatForm);
+  InvCartesiant=matrix_to_cartesiantor (InvMatForm);
   endfunction
 
 function PointOffset= calc_point_offset(RF,FP)
   #Point offset=inv(RF):FP
-  RF_mat=vec_to_matrix(RF);
+  RF_mat=cartesian_to_matrix(RF);
   InvRF_mat=inv(RF_mat);
-  InvRF=matrix_to_vector(InvRF_mat);
-  PointOffset_mat=vec_multiplication(InvRF,FP);
-  PointOffset=matrix_to_vector(PointOffset_mat);
+  InvRF=matrix_to_cartesiantor(InvRF_mat);
+  PointOffset_mat=cartesian_multiplication(InvRF,FP);
+  PointOffset=matrix_to_cartesiantor(PointOffset_mat);
   endfunction
 
 
@@ -125,5 +125,5 @@ PointOffset=calc_point_offset(RF,FP)
 
 P2=[-1.318	-0.425	1.774	-179.998	-5.151	-180];
 P2_Shifted=[9.204	9.651	1.115	-179.98	-10.327	179.999];
-InvP2=inverse_vector(P2);
-offsetPickPose=multiply_two_vectors(InvP2,P2_Shifted)
+InvP2=inverse_cartesiantor(P2);
+offsetPickPose=multiply_two_cartesiantors(InvP2,P2_Shifted)
